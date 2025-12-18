3dee1aa433067af06ea1df9f97710737f7986640
AdamTabiev <tabievadam@gmail.com>
2025-12-18 12:38:46 +0100

    The first and Initial settings of the project

---
+++ RSSReader/RSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSReaderApp.swift
+//  RSSReader
+//
+//  Created by Адам Табиев on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct RSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
